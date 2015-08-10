4b68d8e4108425ff72ccb4cb9bad9093567fdfdf
Andy Yu Zhu Yao <ay3@williams.edu>
2015-08-10 14:14:48 -0400

    all checks finally passed...

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,6 @@
 .onLoad <- function(libname, pkgname) {
   
   ## compress the vignette PDF to fix CMD Check WARNING
-  ## The below works and please don't delete this!
   
   Sys.setenv("_R_BUILD_COMPACT_VIGNETTES_" = "gs+qpdf")
 }
